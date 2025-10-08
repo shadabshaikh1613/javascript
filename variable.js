8978317471d844973c001755a6e01057a92c9f31
Shadab Rais Sahikh <shaikhshadab1613@gmail.com>
2025-10-08 14:30:06 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,29 @@
+//variable-> container to store data/value
+/*
+1. string
+2. number
+3. boolean
+4. object -> null
+5. undefined 
+*/
+
+FullName = "SPRK";
+console.log("Name",FullName);
+console.log(" Type Name", typeof FullName);
+
+
+FullName = 50;
+console.log("Name",FullName);
+console.log(" Type Name", typeof FullName);
+
+FullName = 5.845874;
+console.log("Name",FullName);
+console.log(" Type Name", typeof FullName);
+
+FullName = ture;
+console.log("Name",FullName);
+console.log(" Type Name", typeof FullName);
+
+FullName = null;
+console.log("Name",FullName);
+console.log(" Type Name", typeof FullName);
---
+++ variable2.js
@@ -0,0 +1,18 @@
+const pi =3.1493743;
+
+console.log("Value of pi =", pi);
+
+//constant cannot br reassigned
+// pi = 9.81;
+// console.log(" value of pi "= pi);
+ // let is used to declare/define a variable
+ //with let keyword we can reassined values
+
+ let num;
+ console.log("value of num is" = num);
+
+ num = 50;
+ console.log("value of num is" = num);
+ // let num =8250; variable with let cannot br redeclare
+ 
+ 
